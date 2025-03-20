cdeac82af8c6294af350220abba32f3d05dfa9c7
Vinayak-Gawade <vinayakgawade35@gmail.com>
2025-03-20 23:54:52 +0530

    Creating spring boot application

---
+++ src/main/java/com/dsa/visualization/DsaVisualizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.visualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaVisualizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaVisualizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsa/visualization/DsaVisualizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.visualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaVisualizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
